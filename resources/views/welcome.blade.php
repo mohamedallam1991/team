da04dc7b5394238283ebb31d6ff2a167a2db8570
Jacques <jvw679@gmail.com>
2017-09-08 19:28:45 +0200

    first frontend push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to Anything and Something App
                 </div>
 
                 <div class="links">
